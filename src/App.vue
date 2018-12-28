aa30bc744da35ca00ac3d450078c54663a583fff
Jonsonice <415410795@qq.com>
2018-12-28 16:52:58 +0800

    创建header

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <template>
   <div id="app">
-
+    <!-- 头部 -->
+    <app-header></app-header>
+    <!-- 导航 -->
+    <div class="nav">
+      导航
+    </div>
+    <!-- 内容   -->
+    <div class="content">
+      内容
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Header from './components/header/Header'
 
 export default {
   name: 'App',
   components: {
-
+    "app-header":Header
   }
 }
 </script>
---
+++ src/components/header/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="header">
+   公共的头部信息
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
