86438b41ff8580e41059e3862aaeae30f5a9411d
Jonsonice <415410795@qq.com>
2018-12-28 20:31:13 +0800

    Goods页 flex布局

--- src/components/goods/Goods.vue
+++ src/components/goods/Goods.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="goods">
-  点评信息
+    <!-- 分类列表 -->
+    <div class="menu-wrapper">
+      
+    </div>
+    <!-- 商品列表 -->
+    <div class="foods-wrapper">
+      
+    </div>
   </div>
 </template>
 
@@ -12,5 +19,22 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.goods{
+  display: flex;
+  position: absolute;
+  top: 190px;
+  bottom: 51px;
+  overflow: hidden;
+  width: 100%;
+}
 
+.goods .menu-wrapper{
+  flex:0 0 85px;
+  background: #f4f4f4;
+}
+
+.goods .foods-wrapper{
+  flex:1;
+  background: blue; 
+}
 </style>
