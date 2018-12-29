6a0cf5215c3174d0e9499d396b9acf6802a1233b
Jonsonice <415410795@qq.com>
2018-12-29 11:14:06 +0800

    Goods页底部购物车样式

--- src/components/shopcart/Shopcart.vue
+++ src/components/shopcart/Shopcart.vue
@@ -3,7 +3,7 @@
     <!-- 底部左侧 -->
     <div class="content-left">
       <div class="logo-wrapper">
-        <span class="icon-shopping_cart"></span>
+        <span class="icon-shopping_cart logo"></span>
       </div>
       <div class="desc-wrapper">
         <p class="tip">另需配送费用</p>
@@ -26,6 +26,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+@import url(../../common/css/icon.css);
+
 .shopcart-wrapper{
   width: 100%;
   height: 51px;
@@ -42,5 +44,36 @@ export default {
 
 .shopcart-wrapper .content-right{
   flex: 0 0 110px;
+  font-size: 15px;
+  color: #BAB9B9;
+  line-height: 51px;
+  text-align: center;
+  font-weight: bold;
+}
+.shopcart-wrapper .content-left .logo-wrapper{
+  width: 50px;
+  height: 50px;
+  background: #666666;
+  border-radius: 50%;
+  position: relative;
+  top: -14px;
+  left: 10px;
+  text-align: center;
+  float: left;
+}
+.shopcart-wrapper .content-left .logo-wrapper .logo{
+  font-size: 28px;
+  color: #c4c4c4;
+  line-height: 50px;
+}
+.shopcart-wrapper .content-left .desc-wrapper{
+  float: left;
+  margin-left: 13px;
+}
+
+.shopcart-wrapper .content-left .desc-wrapper .tip{
+  font-size: 12px;
+  color: #bab9b9;
+  line-height: 51px;
 }
 </style>
