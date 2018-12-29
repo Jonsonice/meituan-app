0e410533a05bda5adfc6b2959c2ac766138602da
Jonsonice <415410795@qq.com>
2018-12-29 16:09:10 +0800

    Goods页购物车遮罩层

--- src/components/shopcart/Shopcart.vue
+++ src/components/shopcart/Shopcart.vue
@@ -1,71 +1,74 @@
 <template>
-  <div class="shopcart-wrapper">
-    <!-- 底部左侧 -->
-    <div class="content-left">
-      <div 
-        class="logo-wrapper" 
-        :class="{'highligh':totalCount>0}">
-        <span 
-          class="icon-shopping_cart logo" 
-          :class="{'highligh':totalCount>0}"
-          @click="toggleList"></span>
-        <i class="num" v-show="totalCount">{{totalCount}}</i>
-      </div>
-      <div class="desc-wrapper">
-        <p class="total-price" v-show="totalPrice">
-          ￥{{totalPrice}}
-        </p>
-        <p 
-          class="tip"
-          :class="{'highligh':totalCount>0}">另需{{poiInfo.shipping_fee_tip}}</p>
-      </div>
-    </div>
-    <!-- 底部右侧 -->
-    <div class="content-right" :class="{'highligh':totalCount>0}">
-      {{payStr}}
-    </div>
-    <!-- 购物车列表 -->
-    <div 
-      class="shopcart-list"
-      v-show="listShow"
-      :class="{'show':listShow}">
-      <div class="list-top" v-if="poiInfo.discounts2">
-        {{poiInfo.discounts2[0].info}}
-      </div>
-      <div class="list-header">
-        <h3 class="title">1号口袋</h3>
-        <div class="empty" @click="clearAll">
-          <img src="./img/ash_bin.png" />
-          <span>清空购物车</span>
+  <div class="shopcart" :class="{'highligh':totalCount>0}">
+    <div class="shopcart-wrapper">
+      <!-- 底部左侧 -->
+      <div class="content-left">
+        <div 
+          class="logo-wrapper" 
+          :class="{'highligh':totalCount>0}">
+          <span 
+            class="icon-shopping_cart logo" 
+            :class="{'highligh':totalCount>0}"
+            @click="toggleList"></span>
+          <i class="num" v-show="totalCount">{{totalCount}}</i>
+        </div>
+        <div class="desc-wrapper">
+          <p class="total-price" v-show="totalPrice">
+            ￥{{totalPrice}}
+          </p>
+          <p 
+            class="tip"
+            :class="{'highligh':totalCount>0}">另需{{poiInfo.shipping_fee_tip}}</p>
         </div>
       </div>
-      <div class="list-content" ref="listContent">
-        <ul>
-          <li 
-            class="food-item"
-            v-for="(food,index) in selectFoods"
-            :key="index"
-            >
-            <div class="desc-wrapper">
-              <div class="desc-left">
-                <p class="name">{{food.name}}</p>
-                <p class="unit" v-show="!food.description">{{food.unit}}</p>
-                <p class="description" v-show="!food.unit">{{food.description}}</p>
-              </div>
-              <div class="desc-right">
-                ￥{{food.min_price}}
-              </div>
-            </div>
-            <div class="cartcontrol-wrapper">
-              <app-cart-control :food="food"></app-cart-control>
-            </div>
-          </li>
-        </ul>
+      <!-- 底部右侧 -->
+      <div class="content-right" :class="{'highligh':totalCount>0}">
+        {{payStr}}
       </div>
-      <div class="list-bottom">
-        
+      <!-- 购物车列表 -->
+      <div 
+        class="shopcart-list"
+        v-show="listShow"
+        :class="{'show':listShow}">
+        <div class="list-top" v-if="poiInfo.discounts2">
+          {{poiInfo.discounts2[0].info}}
+        </div>
+        <div class="list-header">
+          <h3 class="title">1号口袋</h3>
+          <div class="empty" @click="clearAll">
+            <img src="./img/ash_bin.png" />
+            <span>清空购物车</span>
+          </div>
+        </div>
+        <div class="list-content" ref="listContent">
+          <ul>
+            <li 
+              class="food-item"
+              v-for="(food,index) in selectFoods"
+              :key="index"
+              >
+              <div class="desc-wrapper">
+                <div class="desc-left">
+                  <p class="name">{{food.name}}</p>
+                  <p class="unit" v-show="!food.description">{{food.unit}}</p>
+                  <p class="description" v-show="!food.unit">{{food.description}}</p>
+                </div>
+                <div class="desc-right">
+                  ￥{{food.min_price}}
+                </div>
+              </div>
+              <div class="cartcontrol-wrapper">
+                <app-cart-control :food="food"></app-cart-control>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div class="list-bottom">
+          
+        </div>
       </div>
     </div>
+    <div class="shopcart-mask" v-show="listShow" @click="hideMask"></div>
   </div>
 </template>
 
@@ -150,6 +153,9 @@
         this.selectFoods.forEach((food) => {
           food.count = 0
         })
+      },
+      hideMask(){
+        this.fold = true
       }
     },
     components:{
@@ -345,5 +351,13 @@
   float: right;
   margin-top: 6px;
 }
-
+.shopcart .shopcart-mask{
+  position: fixed;
+  top: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 98;
+  background: rgba(7,17,27,0.6);
+}
 </style>
