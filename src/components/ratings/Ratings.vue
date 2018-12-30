b6f1803fc99985d2b80d2de21500ec1b44831815
Jonsonice <415410795@qq.com>
2018-12-30 11:44:37 +0800

    评价页引入分隔条组件星星组件

--- src/components/ratings/Ratings.vue
+++ src/components/ratings/Ratings.vue
@@ -1,13 +1,48 @@
 <template>
   <div class="ratings">
-	评价信息
+	 <div class="ratings-wrapper">
+      <div class="overview">
+        <div class="overview-left">
+          <div class="comment-score">
+            <p class="score"></p>
+            <p class="text">商家评分</p>
+          </div>
+          <div class="other-score">
+            <div class="quality-score item">
+              <span class="text">口味</span>
+              <Star class='star'></Star>
+              <span class="score"></span>
+            </div>
+            <div class="pack-score item">
+              <span class="text">包装</span>
+              <Star class='star'></Star>
+              <span class="score"></span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="overview-right">
+        
+      </div>
+    </div>
+    <Split></Split>
+
+    <div class="content">
+
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+  import Split from '../split/Split'
+  import Star from '../star/Star'
 
-}
+  export default {
+    components:{
+      Split,
+      Star
+    }
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
